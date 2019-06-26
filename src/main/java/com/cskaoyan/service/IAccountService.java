06ed28726afaa9dc4bb59bd89ee3a2c17a000938
weiweimao <506586963@qq.com>
2019-06-26 20:34:05 +0800

    最终框架

--- src/main/java/com/cskaoyan/service/IAccountService.java
+++
@@ -1,7 +0,0 @@
-package com.cskaoyan.service;
-
-import com.cskaoyan.bean.Account;
-
-public interface IAccountService {
-    void transcation(Integer a,Integer b);
-}
--- src/main/java/com/cskaoyan/service/IDepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.cskaoyan.service;
-
-import com.cskaoyan.bean.Department;
-
-import java.util.List;
-
-public interface IDepartmentService {
-    List<Department> findAll();
-}
