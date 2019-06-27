33a2142343bb9d2e856aaea4ca8f33706c76dcf3
IL-M <simple_4_w@163.com>
2019-06-27 22:37:11 +0800

    物料信息新增

--- src/main/java/com/cskaoyan/controller/material/MaterialMonitorController.java
+++ src/main/java/com/cskaoyan/controller/material/MaterialMonitorController.java
@@ -1,9 +1,7 @@
 package com.cskaoyan.controller.material;
 
-import com.cskaoyan.bean.material.Material;
-import com.cskaoyan.bean.material.MaterialConsume;
-import com.cskaoyan.bean.material.MaterialReceive;
-import com.cskaoyan.bean.material.MaterialReceives;
+import com.cskaoyan.bean.Page;
+import com.cskaoyan.bean.material.*;
 import com.cskaoyan.mapper.material.MaterialMapper;
 import com.cskaoyan.service.material.MaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -20,48 +21,94 @@ public class MaterialMonitorController {
     @Autowired
     MaterialService materialService;
 
+    @Autowired
+    HttpSession session;
+
     //物料信息
     @RequestMapping("/material/find")
     public ModelAndView findMaterial(){
         ModelAndView modelAndView = new ModelAndView();
+        String[] sysPermissionList = {"material:add","material:edit","material:delete"};
+        session.setAttribute("sysPermissionList",sysPermissionList);
         modelAndView.setViewName("/WEB-INF/jsp/material_list.jsp");
         return modelAndView;
     }
     @RequestMapping("/material/list")
     @ResponseBody
-    public List<Material> materialList(){
-        List<Material> materials = materialService.queryMaterialList();
-        System.out.println(materials);
-        return materials;
+    public Page<Material> MaterialList(int page, int rows){
+
+        Page<Material> materialPage = materialService.selectMaterialPage(page, rows);
+        return materialPage;
     }
+    //物料新增判断
+    @RequestMapping("/material/add_judge")
+    @ResponseBody
+    public String addMaterialjudge(){
+        return "{}";
+    }
+    //物料新增页面跳转
+    @RequestMapping("/material/add")
+    public ModelAndView addMaterial(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/WEB-INF/jsp/material_add.jsp");
+        return modelAndView;
+    }
+    //物料新增到数据库
+    @RequestMapping("/material/insert")
+    @ResponseBody
+    public Map<String,Object> insertMaterial(Material material){
+        int i = materialService.insertMaterial(material);
+        Map<String, Object> map = new HashMap<>();
+        if(i==1){
+            map.put("status",200);
+            map.put("msg","ok");
+        }else{
+            map.put("data","error");
+        }
+        return map;
+    }
+    //物料删除确认
+    @RequestMapping("material/delete_judge")
+    @ResponseBody
+    public String deleteMaterialConfirm(){
+        return "{}";
+    }
+   /* //物料从数据库删除
+    @RequestMapping("material/delete_batch")
+    @ResponseBody
+    public Map<String,Object> deleteMaterials(List<Material> materials){
+
+    }*/
 
     //物料收入
     @RequestMapping("/materialReceive/find")
     public ModelAndView findMaterialReceive(){
         ModelAndView modelAndView = new ModelAndView();
+        String[] sysPermissionList = {"materialReceive:add","materialReceive:edit","materialReceive:delete"};
+        session.setAttribute("sysPermissionList",sysPermissionList);
         modelAndView.setViewName("/WEB-INF/jsp/materialReceive_list.jsp");
         return modelAndView;
     }
     @RequestMapping("/materialReceive/list")
     @ResponseBody
-    public Map<String, Object> materialReceiveList(){
-        Map<String, Object> materialReceives = materialService.allMaterialReceives();
-        System.out.println(materialReceives);
-        return materialReceives;
+    public Page<MaterialReceives> MaterialReceives(int page, int rows){
+        Page<MaterialReceives> materialReceivesPage = materialService.selectMaterialReceivesPage(page, rows);
+        return materialReceivesPage;
     }
     //物料消耗
     @RequestMapping("/materialConsume/find")
     public ModelAndView findMaterialConsume(){
         ModelAndView modelAndView = new ModelAndView();
+        String[] sysPermissionList = {"materialConsume:add","materialConsume:edit","materialConsume:delete"};
+        session.setAttribute("sysPermissionList",sysPermissionList);
         modelAndView.setViewName("/WEB-INF/jsp/materialConsume_list.jsp");
         return modelAndView;
     }
     @RequestMapping("/materialConsume/list")
     @ResponseBody
-    public Map<String, Object> materialConsumeList(){
-        Map<String, Object> consumes = materialService.allMaterialConsumes();
-        System.out.println(consumes);
-        return consumes;
+    public Page<MaterialConsumes> MaterialConsumes(int page, int rows){
+        Page<MaterialConsumes> materialConsumesPage = materialService.selectMaterialConsumesPage(page,rows);
+        return materialConsumesPage;
     }
     @Autowired
     MaterialMapper materialMapper;
--- src/main/java/com/cskaoyan/service/material/impl/MaterialServiceImpl.java
+++ src/main/java/com/cskaoyan/service/material/impl/MaterialServiceImpl.java
@@ -1,10 +1,12 @@
 package com.cskaoyan.service.material.impl;
 
+import com.cskaoyan.bean.Page;
 import com.cskaoyan.bean.material.*;
 import com.cskaoyan.mapper.material.MaterialConsumeMapper;
 import com.cskaoyan.mapper.material.MaterialMapper;
 import com.cskaoyan.mapper.material.MaterialReceiveMapper;
 import com.cskaoyan.service.material.MaterialService;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,51 +19,43 @@ public class MaterialServiceImpl implements MaterialService {
     @Autowired
     MaterialMapper materialMapper;
 
+    //物料信息分页查询
     @Override
-    public List<Material> queryMaterialList() {
+    public Page<Material> selectMaterialPage(int page, int rows) {
+        Page<Material> selectMaterialPage = new Page<>();
+        PageHelper.startPage(page,rows);
         MaterialExample materialExample = new MaterialExample();
-        MaterialExample.Criteria criteria = materialExample.createCriteria();
-
-        return materialMapper.selectByExample(materialExample);
+        List<Material> materials = materialMapper.selectByExample(materialExample);
+        selectMaterialPage.setRows(materials);
+        selectMaterialPage.setTotal(materials.size());
+        return selectMaterialPage;
     }
-
-
-    @Autowired
-    MaterialReceiveMapper materialReceiveMapper;
+    //物料收入分页查询
     @Override
-    public Map<String, Object> allMaterialReceives() {
-        MaterialReceiveExample materialReceiveExample = new MaterialReceiveExample();
+    public Page<MaterialReceives> selectMaterialReceivesPage(int page, int rows) {
+        Page<MaterialReceives> selectMaterialReceivesPage = new Page<>();
+        PageHelper.startPage(page,rows);
+       // MaterialReceiveExample materialReceiveExample = new MaterialReceiveExample();
         List<MaterialReceives> materialReceives = materialMapper.allMaterialReceives();
-        long l = materialReceiveMapper.countByExample(materialReceiveExample);
-        Map<String, Object> map = new HashMap<>();
-        map.put("total",l);
-        map.put("rows",materialReceives);
-        return map;
-        /*
-        MaterialReceiveExample.Criteria criteria = materialReceiveExample.createCriteria();
-
-
-       */
+        selectMaterialReceivesPage.setRows(materialReceives);
+        selectMaterialReceivesPage.setTotal(materialReceives.size());
+        return selectMaterialReceivesPage;
     }
-
+    //物料消耗分页查询
     @Override
-    public Map<String, Object> allMaterialConsumes() {
-        MaterialConsumeExample materialConsumeExample = new MaterialConsumeExample();
+    public Page<MaterialConsumes> selectMaterialConsumesPage(int page, int rows) {
+        Page<MaterialConsumes> selectMaterialConsumesPage = new Page<>();
+        PageHelper.startPage(page,rows);
         List<MaterialConsumes> materialConsumes = materialMapper.allMaterialConsumes();
-        long l = materialConsumeMapper.countByExample(materialConsumeExample);
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("total",l);
-        map.put("rows",materialConsumes);
-        return map;
+        selectMaterialConsumesPage.setRows(materialConsumes);
+        selectMaterialConsumesPage.setTotal(materialConsumes.size());
+        return selectMaterialConsumesPage;
     }
-
-    @Autowired
-    MaterialConsumeMapper materialConsumeMapper;
+    //物料信息新增
     @Override
-    public List<MaterialConsume> queryMaterialConsumeList() {
-        MaterialConsumeExample materialConsumeExample = new MaterialConsumeExample();
-        MaterialConsumeExample.Criteria criteria = materialConsumeExample.createCriteria();
-
-        return materialConsumeMapper.selectByExample(materialConsumeExample);
+    public int insertMaterial(Material material) {
+        MaterialExample materialExample = new MaterialExample();
+        int insert = materialMapper.insert(material);
+        return insert;
     }
 }
